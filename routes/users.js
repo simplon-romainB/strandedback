daa192e505eb80e56ce5bb06126631bc0a2a7a21
simplon-romainB <yysh@hotmail.fr>
2020-01-13 14:36:44 +0100

    ok

--- routes/users.js
+++ routes/users.js
@@ -10,16 +10,16 @@ const bcrypt = require('bcryptjs');
 
 /* GET users listing. */
 router.post('/', async(req, res, next) => {
-  var requ = JSON.parse(req.body)
+  console.log(req)
   var salt = bcrypt.genSaltSync(10);
-  var hash = bcrypt.hashSync(requ.body.password, salt);
+  var hash = bcrypt.hashSync(req.body.password, salt);
   await bcrypt.genSalt(10, async(err,salt) =>
-  await bcrypt.hash(requ.body.password, salt, function(err, hash){
+  await bcrypt.hash(req.body.password, salt, function(err, hash){
 
     })
   )
   const request = "INSERT INTO user (user_id,user_name,user_email) VALUES (DEFAULT,$1,$2,$3 )"
-  const args = [requ.body.name, requ.body.email,hash]
+  const args = [req.body.name, req.body.email,hash]
   const client = await pool.connect()
   const result = await client.query(request,args,(err,result) => {
     res.send(result)

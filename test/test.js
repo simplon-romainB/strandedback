6e1153d91c4277483bede463d327854482ebbf99
unknown <romain.barry69@gmail.com>
2020-03-21 18:11:11 +0100

    premierstests

--- test/test.js
+++ test/test.js
@@ -4,14 +4,18 @@ var express = require('express');
 var should = chai.should();
 //var sinon = require('sinon');
 
+chai.use(chaiHttp);
+
 
 describe('unitTest', () => {
 
     it('Main page status', function (done) {
         chai.request('https://peaceful-mountain-88307.herokuapp.com/articles').get('/')
-        //chai.request('https://peaceful-mountain-88307.herokuapp.com/articles', function (error, response, body) {
+        done()
+        chai.request('https://peaceful-mountain-88307.herokuapp.com/articles', function (error, response, body) {
         expect(request.method).to.Be(GET)  
         expect(response.statusCode).to.equal(200);
             done();
         });
     });
+});
